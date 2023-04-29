39da2713c10d9f2affb3dcfe0156e26420fad662
Swati Jangra <111527689+SwatiJangra@users.noreply.github.com>
2023-04-29 15:23:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const container = document.querySelector('.container');
+
+// Generate 800 squares dynamically
+for (let i = 0; i < 800; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  square.addEventListener('mouseenter', () => {
+    square.style.backgroundColor = 'rgb(29, 29, 29';
+  });
+  square.addEventListener('mouseleave', () => {
+    setTimeout(() => {
+      square.style.backgroundColor = 'rgb(29, 29, 29)';
+    }, 1000);
+  });
+  container.appendChild(square);
+}
