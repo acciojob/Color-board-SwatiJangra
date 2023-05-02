a10179c71f8e98ce97cd3c6ba0923ca27074290d
Swati Jangra <111527689+SwatiJangra@users.noreply.github.com>
2023-05-02 16:20:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-//your JS code here. If required.
-//your JS code here. If required.
-const container = document.querySelector('.container');
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+// const colors = ['#ff7f50','#87cefa','#da70d6','#32cd32','#6495ed','#ff69b4','#ba55d3','#cd5c5c','#ffa500','#40e0d0']
 
-// Generate 800 squares dynamically
-for (let i = 0; i < 800; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  square.addEventListener('mouseenter', () => {
-    square.style.backgroundColor = 'rgb(29, 29, 29';
-  });
-  square.addEventListener('mouseleave', () => {
-    setTimeout(() => {
-      square.style.backgroundColor = 'rgb(29, 29, 29)';
-    }, 1000);
-  });
-  container.appendChild(square);
+
+const container  = document.getElementById("container")
+
+const SQUARES = 800
+
+for(let i=0;i<SQUARES;i++){
+    const square = document.createElement("div")
+    square.classList.add("square")
+
+    square.addEventListener("mouseover",()=> setColor(square))
+    square.addEventListener("mouseout",()=> removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element){
+    const color = randomColor()
+
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+
+}
+
+function removeColor(element){
+    element.style.background = "#1d1d1d"
+    element.style.boxShadow = "0 0 2px #000"
+}
+function randomColor(){
+    const index = Math.floor(Math.random()*colors.length)
+    return colors[index]
 }
